00c830e610ce9b44e5d8cee1f79d564fb95d4e69
Kobayashi Ruma <otoutosama321@gmail.com>
2026-05-28 15:24:32 +0900

    feat: お問い合わせフォームのバリデーション、確認画面、送信・保存処理の実装完了

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// PG01: お問い合わせフォーム入力ページ
+Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+// PG02: お問い合わせフォーム確認ページ
+Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
+// お問い合わせ送信処理
+Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
+// PG03: サンクスページ
+Route::get('/thanks', [ContactController::class, 'thanks'])->name('contact.thanks');
